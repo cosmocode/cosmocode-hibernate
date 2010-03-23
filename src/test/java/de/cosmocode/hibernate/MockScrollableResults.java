523452a9d9054e6947f39d99bb607526c2b0acbf
schoenborn <schoenborn@caa2ac01-1b1e-4351-a100-d44f7e30510d>
2010-03-23 11:08:49 +0000

    added svn:ignore values, added MockScrollableResults to test ScrollableResultsIterator

---
+++ src/test/java/de/cosmocode/hibernate/MockScrollableResults.java
@@ -0,0 +1,211 @@
+package de.cosmocode.hibernate;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Blob;
+import java.sql.Clob;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import org.hibernate.HibernateException;
+import org.hibernate.ScrollableResults;
+import org.hibernate.type.Type;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * Mock implementation of the {@link ScrollableResults} which uses
+ * an underlying list.
+ *
+ * @author Willi Schoenborn
+ */
+final class MockScrollableResults implements ScrollableResults {
+
+    private final List<?> list;
+    
+    private int index = -1;
+    
+    public <T> MockScrollableResults(List<T> list) {
+        this.list = Preconditions.checkNotNull(list, "List");
+    }
+    
+    @Override
+    public void afterLast() throws HibernateException {
+        index = list.size();
+    }
+
+    @Override
+    public void beforeFirst() throws HibernateException {
+        index = -1;
+    }
+
+    @Override
+    public void close() throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean first() throws HibernateException {
+        index = 0;
+        return list.size() > 0;
+    }
+
+    @Override
+    public Object get(int i) throws HibernateException {
+        return list.get(index);
+    }
+
+    @Override
+    public boolean isFirst() throws HibernateException {
+        return index == 0;
+    }
+
+    @Override
+    public boolean isLast() throws HibernateException {
+        return index == list.size() - 1;
+    }
+
+    @Override
+    public boolean last() throws HibernateException {
+        index = list.size() - 1;
+        return list.size() > 0;
+    }
+
+    @Override
+    public boolean next() throws HibernateException {
+        index++;
+        return index < list.size();
+    }
+
+    @Override
+    public boolean previous() throws HibernateException {
+        index -= 1;
+        return index < list.size();
+    }
+
+    @Override
+    public boolean scroll(int i) throws HibernateException {
+        index += i;
+        return index < list.size();
+    }
+
+    @Override
+    public int getRowNumber() throws HibernateException {
+        return index;
+    }
+
+    @Override
+    public boolean setRowNumber(int rowNumber) throws HibernateException {
+        index = rowNumber;
+        return index < list.size();
+    }
+
+    @Override
+    public Object[] get() throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public BigDecimal getBigDecimal(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public BigInteger getBigInteger(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public byte[] getBinary(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Blob getBlob(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Boolean getBoolean(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Byte getByte(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Calendar getCalendar(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Character getCharacter(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Clob getClob(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Date getDate(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Double getDouble(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Float getFloat(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Integer getInteger(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Locale getLocale(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Long getLong(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Short getShort(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getString(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getText(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public TimeZone getTimeZone(int col) throws HibernateException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Type getType(int i) {
+        throw new UnsupportedOperationException();
+    }
+
+}
--- src/test/java/de/cosmocode/hibernate/ScrollableResultsIteratorTest.java
+++ src/test/java/de/cosmocode/hibernate/ScrollableResultsIteratorTest.java
@@ -1,33 +1,47 @@
 package de.cosmocode.hibernate;
 
+import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Iterator;
 
 import org.easymock.EasyMock;
 import org.hibernate.ScrollableResults;
 import org.junit.Assert;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+/**
+ * Tests {@link ScrollableResultsIterator} using {@link MockScrollableResults}.
+ *
+ * @author Willi Schoenborn
+ */
 public class ScrollableResultsIteratorTest {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ScrollableResultsIteratorTest.class);
-
-    private Iterator<?> unit(ScrollableResults results) {
-        return new ScrollableResultsIterator<Object>(results);
+    private <T> Iterator<T> unit(ScrollableResults results) {
+        return new ScrollableResultsIterator<T>(results);
     }
     
+    private <T> Iterator<T> unit(T... elements) {
+        return unit(new MockScrollableResults(Arrays.asList(elements)));
+    }
+    
+    /**
+     * Tests a usual iterator usage.
+     */
     @Test
-    public void next() {
-        final ScrollableResults results = EasyMock.createMock(ScrollableResults.class);
-        final Object first = new Object();
-        EasyMock.expect(results.isLast()).andStubReturn(false);
-        EasyMock.expect(results.next()).andReturn(true);
-        EasyMock.expect(results.get(0)).andReturn(first);
-        EasyMock.replay(results);
-        final Iterator<?> iterator = unit(results);
+    public void test() {
+        final Object first = EasyMock.createMock("first", Serializable.class);
+        final Object second = EasyMock.createMock("second", Serializable.class);
+        final Object third = EasyMock.createMock("third", Serializable.class);
+        final Iterator<Object> iterator = unit(first, second, third);
+        
+        Assert.assertNotNull(iterator);
         Assert.assertTrue(iterator.hasNext());
-        Assert.assertEquals(first, iterator.next());
+        Assert.assertSame(first, iterator.next());
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertSame(second, iterator.next());
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertSame(third, iterator.next());
+        Assert.assertFalse(iterator.hasNext());
     }
     
 }
